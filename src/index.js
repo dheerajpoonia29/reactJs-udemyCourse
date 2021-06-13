77213e8071be0aed5a62afb3b2d342733337a3a7
dheerajpoonia29 <dheerajbishnoi123@gmail.com>
2021-06-13 14:02:25 +0530

    init react js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+   <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+      </header>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
